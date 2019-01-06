6d24907dc4eb6033fbb2e606302df4d6b6d58adb
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-06 15:31:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,8 +38,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element)
   
 function removeElementFromBeginningOfArray(array)
   {
-    return array.slice(0)
-    
+    return array.slice(1)
   }
   
 function destructivelyRemoveElementFromEndOfArray(array)
