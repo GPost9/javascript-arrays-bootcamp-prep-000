6c1fbcf6444a7c49826cd0960144d3d21caec881
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-06 15:35:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -49,5 +49,5 @@ function destructivelyRemoveElementFromEndOfArray(array)
   
 function removeElementFromEndOfArray(array)
   {
-    array.slice(0, array.length -1)
+    return array.slice(0, array.length -1)
   }
