5fb4e06e3995b4282435292d8f86a9209bb564ab
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-06 15:21:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function addElementToBeginningOfArray(array, element)
   
 function destructivelyAddElementToBeginningOfArray(array, element)
     {
-    return array.unshift(element) 
+    return element.unshift(array) 
     }
   
 function addElementToEndOfArray(array, element)
